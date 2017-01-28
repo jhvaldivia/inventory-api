36748a925ab85e616054f100465b9e0f0b77d29e
Jaime Valdivia <jaivaldivia@gmail.com>
2017-01-28 14:39:34 -0600

    Adding pagination support for the products API

--- src/main/java/com/kolimpri/ag/repo/ProductRepository.java
+++ src/main/java/com/kolimpri/ag/repo/ProductRepository.java
@@ -1,12 +1,12 @@
 package com.kolimpri.ag.repo;
 
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.kolimpri.ag.domain.Product;
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-	List<Product> findByProductOrderCode(@Param("code") String code);
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+	Page<Product> findByProductOrderCode(@Param("code") String code, Pageable pageable);
 }
