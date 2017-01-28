7f615ebd6fac9788821790b17868f4974c4ba97f
Jaime Valdivia <jaivaldivia@gmail.com>
2017-01-28 14:30:41 -0600

    Exposing search resource on the apis

--- src/main/java/com/kolimpri/ag/repo/ProductOrderRepository.java
+++ src/main/java/com/kolimpri/ag/repo/ProductOrderRepository.java
@@ -1,9 +1,11 @@
 package com.kolimpri.ag.repo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.kolimpri.ag.domain.ProductOrder;
 
 public interface ProductOrderRepository extends CrudRepository<ProductOrder, String> {
-
+	
+	ProductOrder findByName(@Param("name") String name);
 }
--- src/main/java/com/kolimpri/ag/repo/ProductRepository.java
+++ src/main/java/com/kolimpri/ag/repo/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.kolimpri.ag.repo;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.kolimpri.ag.domain.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
-
+	List<Product> findByProductOrderCode(@Param("code") String code);
 }
