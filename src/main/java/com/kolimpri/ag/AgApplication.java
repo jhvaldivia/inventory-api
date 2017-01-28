093c644966b06b0e2f7e020426d02ffd06d21c71
Jaime Valdivia <jaivaldivia@gmail.com>
2017-01-28 12:38:55 -0600

    Initial project setup

---
+++ src/main/java/com/kolimpri/ag/AgApplication.java
@@ -0,0 +1,12 @@
+package com.kolimpri.ag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kolimpri/ag/AgApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kolimpri.ag;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
